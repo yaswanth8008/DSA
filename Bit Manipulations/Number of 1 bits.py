89f961d9114dfe5ac913d558bacf474512ce6eae
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-02-16 11:49:37 +0530

    no.of set bits

--- Bit Manipulations/Number of 1 bits.py
+++ Bit Manipulations/Number of 1 bits.py
@@ -3,11 +3,9 @@
 
 # Write a function that takes an integer and returns the number of 1 bits present in its binary representation.
 
-def solve(A):
-    max_till_i = -1000000000
+def numSetBits(self, A):
     cnt = 0
-    for i in range(len(A)):
-        max_till_i = max(A[i],max_till_i)
-        if max_till_i == i:
+    for i in range(32):
+        if A >> i & 1 == 1:
             cnt += 1
-    return cnt
+    return cnt 
