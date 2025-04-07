db52bb13ce3e63a102c53cbe3aa8b2d0fa753b21
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-04-08 00:45:45 +0530

    updated-largest coprime

--- Math/12. Largest Coprime Divisor.py
+++ Math/12. Largest Coprime Divisor.py
@@ -2,3 +2,18 @@
 
 # X divides A i.e. A % X = 0
 # X and B are co-prime i.e. gcd(X, B) = 1
+
+
+
+def cpFact(A, B):
+    def gcd(a,b):
+        if b == 0:
+            return a 
+        else:
+            return gcd(b,a%b)
+    if A == B:
+        return 1
+    else:
+        while gcd(A,B) != 1:
+            A = A//gcd(A,B)
+        return A
