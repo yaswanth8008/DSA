ace0de8fd80c2fd640717ed634bab6db4240b8e6
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-04-09 00:16:24 +0530

    lucky number

--- Math/13. Lucky Number.py
+++ Math/13. Lucky Number.py
@@ -1,2 +1,25 @@
 # A lucky number is a number that has exactly 2 distinct prime divisors.
 # You are given a number A, and you need to determine the count of lucky numbers between the range 1 to A (both inclusive).
+
+
+
+
+
+def solve(A):
+    n = A+1
+    pf_ar = [0]*n
+    for i in range(1,n):
+        pf_ar[i] = i 
+    pf_ar1 = [0]*n
+    for i in range(2,n):
+        if pf_ar[i] == i:
+            for k in range(2*i,n,i):
+                pf_ar[k] = i
+                pf_ar1[k] += 1
+    cnt = 0
+    for i in range(1,n):
+        if pf_ar1[i] == 2:
+            cnt += 1
+    return cnt
+
+solve(21)
