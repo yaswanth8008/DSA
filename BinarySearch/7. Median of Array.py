db5cf12da1cd106154967a39d8cd321a03ea3291
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-04-25 19:32:45 +0530

    gseorgjs

---
+++ BinarySearch/7. Median of Array.py
@@ -0,0 +1,9 @@
+# There are two sorted arrays A and B of sizes N and M respectively.
+
+# Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
+
+# NOTE:
+
+# The overall run time complexity should be O(log(m+n)).
+# IF the number of elements in the merged array is even, then the median is the average of (n/2)th and (n/2+1)th element. 
+# For example, if the array is [1 2 3 4], the median is (2 + 3) / 2.0 = 2.5.
