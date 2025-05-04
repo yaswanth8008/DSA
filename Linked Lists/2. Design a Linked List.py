c60dea69e736391fd26bf64b3f65292d1670f06f
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-05-04 18:53:10 +0530

    LL

---
+++ Linked Lists/2. Design a Linked List.py
@@ -0,0 +1,8 @@
+# Design and implement a Linked List data structure.
+# A node in a linked list should have the following attributes - an integer value and a pointer to the next node.
+
+# It should support the following operations:
+
+# insert_node(position, value) - To insert the input value at the given position in the linked list.
+# delete_node(position) - Delete the value at the given position from the linked list.
+# print_ll() - Print the entire linked list, such that each element is followed by a single space (no trailing spaces).
