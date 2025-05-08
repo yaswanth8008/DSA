d4d8a99b68702e34af7ef9ca3932b90671568ed8
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-05-08 23:59:25 +0530

    uhfwei

---
+++ Hashing/11. Compare sorted sub arrays.py
@@ -0,0 +1,7 @@
+# Given an array A of length N. You have to answer Q queries.
+
+
+
+# Each query will contain four integers l1, r1, l2, and r2. If sorted segment from [l1, r1] is the same as the sorted segment from [l2 r2], then the answer is 1 else 0.
+
+# NOTE The queries are 0-indexed.
