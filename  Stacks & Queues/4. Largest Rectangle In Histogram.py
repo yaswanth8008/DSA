871f0a79b9b85c1adddce0c343bbbc00b6093946
yaswanth8008 <yaswanthkumarseela@gmail.com>
2025-05-14 23:40:04 +0530

    largest rectangle in histogram

---
+++  Stacks & Queues/4. Largest Rectangle In Histogram.py
@@ -0,0 +1,6 @@
+# Given an array of integers A.
+
+
+# A represents a histogram i.e A[i] denotes the height of the ith histogram's bar. Width of each bar is 1.
+
+# Find the area of the largest rectangle formed by the histogram.
